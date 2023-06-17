b14e79a56c96a41046d8a41c3dbf5855e4583a77
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-06-17 13:59:07 +0300

    UI

--- brand-app/src/App.js
+++ brand-app/src/App.js
@@ -46,32 +46,39 @@ function App() {
 
   }
   return (
-    <>
-      <div>
-        <div className='flex-grow p-6'></div>
-        <div className='flex flex-col space-y-4'></div>
+  <div className='container flex'>
+    <div className="border border-solid border-2 w-[400px] h-screen  bg-gradient-to-b from-pink-200 to-white-500-opacity-20 backdrop-filter backdrop-blur-lg p-4 rounded-lg">
+      <h1>Chat History</h1>
+    </div>
+    <div className='container ml-20 mt-30 p-10'>
+      <h1 className='text-xl font-bold text-center'>Your Brand Assistant</h1><br></br>
+      <p className='text-center'>tell me a little bit about your brand</p>
+      <div className='flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        <div class="bg-white rounded-lg shadow-md p-4">
+          <h2 class="text-xl font-bold mb-2">Keywords</h2>
+          <p class="text-gray-600">'give me keywords for my brand catkits'.</p>
+        </div>
+        <div class="bg-white rounded-lg shadow-md p-4">
+          <h2 class="text-xl font-bold mb-2">SEO</h2>
+          <p class="text-gray-600">'how to optimize my website</p>
+        </div>
+        <div class="bg-white rounded-lg shadow-md p-4">
+          <h2 class="text-xl font-bold mb-2">Marketing strategies</h2>
+          <p class="text-gray-600">generate steps to improve leads</p>
       </div>
-      {
-        chatLog.map((message, index) => (
-          <div key={index} className={`flex ${
-            message.type === 'user' ? 'justify-end' : 'justify-start'
-            }`}>
-            <div className={`${
-              message.type === 'user' ? 'bg-white-500' : 'bg-gray-800'
-            } rounded-lg p-4 text-black max-w-sm`}>
-            {message.message}
-            </div>
-            </div>
-        ))}
-      <form class="wrapper" onSubmit={handleSubmit}> 
-        <h1>Brand-assi</h1>
-        <p>tell me what your brand is about and <br></br>I'll generate keywords for you</p>
-        <input placeholder="cat kits" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/><br></br>
-        <button>send</button>
+        
+    </div>
+      <form className='flex pt-10'>
+        <input placeholder='type your message'className='border boader-gray-solid-1 w-[100%] h-20'  />
+        
+        <button className='h-20 bg-gradient-to-b from-pink-200 to-pink-500-opacity-20 backdrop-filter backdrop-blur-lg p-4 rounded-lg' type='submit'>send</button>
       </form>
-    </>
     
-  );
+  </div>
+      
+  </div>
+  
+  )
 }
 
 export default App;
